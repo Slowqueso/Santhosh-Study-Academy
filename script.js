1694a9d4fbc56629783ff2eb5a24661a00c0ba5b
Sanjana Dubey <spdubey6178@gmail.com>
2023-06-28 20:53:57 +0530

    Navbar Responsive

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("hi");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
